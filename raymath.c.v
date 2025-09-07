f218c981d5c20448e23eebf0194de3f3f40a63ab
alfu32 <ioan.alferaru@gmail.com>
2025-09-07 05:54:10 +0200

    fix(raymath): fixed names

--- raymath.c.v
+++ raymath.c.v
@@ -37,187 +37,187 @@ pub fn wrap(value f32, min f32, max f32) f32
 pub fn float_equals(x f32, y f32) int
 
 @[c: 'Vector2Zero']
-pub fn vector2Zero() Vector2
+pub fn vector2_zero() Vector2
 
 @[c: 'Vector2One']
-pub fn vector2One() Vector2
+pub fn vector2_one() Vector2
 
 @[c: 'Vector2Add']
-pub fn vector2Add(v1 Vector2, v2 Vector2) Vector2
+pub fn vector2_add(v1 Vector2, v2 Vector2) Vector2
 
 @[c: 'Vector2AddValue']
-pub fn vector2AddValue(v Vector2, add f32) Vector2
+pub fn vector2_add_value(v Vector2, add f32) Vector2
 
 @[c: 'Vector2Subtract']
-pub fn vector2Subtract(v1 Vector2, v2 Vector2) Vector2
+pub fn vector2_subtract(v1 Vector2, v2 Vector2) Vector2
 
 @[c: 'Vector2SubtractValue']
-pub fn vector2SubtractValue(v Vector2, sub f32) Vector2
+pub fn vector2_subtract_value(v Vector2, sub f32) Vector2
 
 @[c: 'Vector2Length']
-pub fn vector2Length(v Vector2) f32
+pub fn vector2_length(v Vector2) f32
 
 @[c: 'Vector2LengthSqr']
-pub fn vector2LengthSqr(v Vector2) f32
+pub fn vector2_length_sqr(v Vector2) f32
 
 @[c: 'Vector2DotProduct']
-pub fn vector2DotProduct(v1 Vector2, v2 Vector2) f32
+pub fn vector2_dot_product(v1 Vector2, v2 Vector2) f32
 
 @[c: 'Vector2Distance']
-pub fn vector2Distance(v1 Vector2, v2 Vector2) f32
+pub fn vector2_distance(v1 Vector2, v2 Vector2) f32
 
 @[c: 'Vector2DistanceSqr']
-pub fn vector2DistanceSqr(v1 Vector2, v2 Vector2) f32
+pub fn vector2_distance_sqr(v1 Vector2, v2 Vector2) f32
 
 @[c: 'Vector2Angle']
-pub fn vector2Angle(v1 Vector2, v2 Vector2) f32
+pub fn vector2_angle(v1 Vector2, v2 Vector2) f32
 
 @[c: 'Vector2Scale']
-pub fn vector2Scale(v Vector2, scale f32) Vector2
+pub fn vector2_scale(v Vector2, scale f32) Vector2
 
 @[c: 'Vector2Multiply']
-pub fn vector2Multiply(v1 Vector2, v2 Vector2) Vector2
+pub fn vector2_multiply(v1 Vector2, v2 Vector2) Vector2
 
 @[c: 'Vector2Negate']
-pub fn vector2Negate(v Vector2) Vector2
+pub fn vector2_negate(v Vector2) Vector2
 
 @[c: 'Vector2Divide']
-pub fn vector2Divide(v1 Vector2, v2 Vector2) Vector2
+pub fn vector2_divide(v1 Vector2, v2 Vector2) Vector2
 
 @[c: 'Vector2Normalize']
-pub fn vector2Normalize(v Vector2) Vector2
+pub fn vector2_normalize(v Vector2) Vector2
 
 @[c: 'Vector2Transform']
-pub fn vector2Transform(v Vector2, mat Matrix) Vector2
+pub fn vector2_transform(v Vector2, mat Matrix) Vector2
 
 @[c: 'Vector2Lerp']
-pub fn vector2Lerp(v1 Vector2, v2 Vector2, amount f32) Vector2
+pub fn vector2_lerp(v1 Vector2, v2 Vector2, amount f32) Vector2
 
 @[c: 'Vector2Reflect']
-pub fn vector2Reflect(v Vector2, normal Vector2) Vector2
+pub fn vector2_reflect(v Vector2, normal Vector2) Vector2
 
 @[c: 'Vector2Rotate']
-pub fn vector2Rotate(v Vector2, angle f32) Vector2
+pub fn vector2_rotate(v Vector2, angle f32) Vector2
 
 @[c: 'Vector2MoveTowards']
-pub fn vector2MoveTowards(v Vector2, target Vector2, maxdistance f32) Vector2
+pub fn vector2_move_towards(v Vector2, target Vector2, maxdistance f32) Vector2
 
 @[c: 'Vector2Invert']
-pub fn vector2Invert(v Vector2) Vector2
+pub fn vector2_invert(v Vector2) Vector2
 
 @[c: 'Vector2Clamp']
-pub fn vector2Clamp(v Vector2, min Vector2, max Vector2) Vector2
+pub fn vector2_clamp(v Vector2, min Vector2, max Vector2) Vector2
 
 @[c: 'Vector2ClampValue']
-pub fn vector2ClampValue(v Vector2, min f32, max f32) Vector2
+pub fn vector2_clamp_value(v Vector2, min f32, max f32) Vector2
 
 @[c: 'Vector2Equals']
-pub fn vector2Equals(p Vector2, q Vector2) int
+pub fn vector2_equals(p Vector2, q Vector2) int
 
 @[c: 'Vector3Zero']
-pub fn vector3Zero() Vector3
+pub fn vector3_zero() Vector3
 
 @[c: 'Vector3One']
-pub fn vector3One() Vector3
+pub fn vector3_one() Vector3
 
 @[c: 'Vector3Add']
-pub fn vector3Add(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_add(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3AddValue']
-pub fn vector3AddValue(v Vector3, add f32) Vector3
+pub fn vector3_add_value(v Vector3, add f32) Vector3
 
 @[c: 'Vector3Subtract']
-pub fn vector3Subtract(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_subtract(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3SubtractValue']
-pub fn vector3SubtractValue(v Vector3, sub f32) Vector3
+pub fn vector3_subtract_value(v Vector3, sub f32) Vector3
 
 @[c: 'Vector3Scale']
-pub fn vector3Scale(v Vector3, scalar f32) Vector3
+pub fn vector3_scale(v Vector3, scalar f32) Vector3
 
 @[c: 'Vector3Multiply']
-pub fn vector3Multiply(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_multiply(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3CrossProduct']
-pub fn vector3CrossProduct(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_cross_product(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3Perpendicular']
-pub fn vector3Perpendicular(v Vector3) Vector3
+pub fn vector3_perpendicular(v Vector3) Vector3
 
 @[c: 'Vector3Length']
-pub fn vector3Length(v Vector3) f32
+pub fn vector3_length(v Vector3) f32
 
 @[c: 'Vector3LengthSqr']
-pub fn vector3LengthSqr(v Vector3) f32
+pub fn vector3_length_sqr(v Vector3) f32
 
 @[c: 'Vector3DotProduct']
-pub fn vector3DotProduct(v1 Vector3, v2 Vector3) f32
+pub fn vector3_dot_product(v1 Vector3, v2 Vector3) f32
 
 @[c: 'Vector3Distance']
-pub fn vector3Distance(v1 Vector3, v2 Vector3) f32
+pub fn vector3_distance(v1 Vector3, v2 Vector3) f32
 
 @[c: 'Vector3DistanceSqr']
-pub fn vector3DistanceSqr(v1 Vector3, v2 Vector3) f32
+pub fn vector3_distance_sqr(v1 Vector3, v2 Vector3) f32
 
 @[c: 'Vector3Angle']
-pub fn vector3Angle(v1 Vector3, v2 Vector3) f32
+pub fn vector3_angle(v1 Vector3, v2 Vector3) f32
 
 @[c: 'Vector3Negate']
-pub fn vector3Negate(v Vector3) Vector3
+pub fn vector3_negate(v Vector3) Vector3
 
 @[c: 'Vector3Divide']
-pub fn vector3Divide(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_divide(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3Normalize']
-pub fn vector3Normalize(v Vector3) Vector3
+pub fn vector3_normalize(v Vector3) Vector3
 
 @[c: 'Vector3OrthoNormalize']
-pub fn vector3OrthoNormalize(v1 &Vector3, v2 &Vector3)
+pub fn vector3_ortho_normalize(v1 &Vector3, v2 &Vector3)
 
 @[c: 'Vector3Transform']
-pub fn vector3Transform(v Vector3, mat Matrix) Vector3
+pub fn vector3_transform(v Vector3, mat Matrix) Vector3
 
 @[c: 'Vector3RotateByQuaternion']
-pub fn vector3RotateByQuaternion(v Vector3, q Quaternion) Vector3
+pub fn vector3_rotate_by_quaternion(v Vector3, q Quaternion) Vector3
 
 @[c: 'Vector3RotateByAxisAngle']
-pub fn vector3RotateByAxisAngle(v Vector3, axis Vector3, angle f32) Vector3
+pub fn vector3_rotate_by_axis_angle(v Vector3, axis Vector3, angle f32) Vector3
 
 @[c: 'Vector3Lerp']
-pub fn vector3Lerp(v1 Vector3, v2 Vector3, amount f32) Vector3
+pub fn vector3_lerp(v1 Vector3, v2 Vector3, amount f32) Vector3
 
 @[c: 'Vector3Reflect']
-pub fn vector3Reflect(v Vector3, normal Vector3) Vector3
+pub fn vector3_reflect(v Vector3, normal Vector3) Vector3
 
 @[c: 'Vector3Min']
-pub fn vector3Min(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_min(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3Max']
-pub fn vector3Max(v1 Vector3, v2 Vector3) Vector3
+pub fn vector3_max(v1 Vector3, v2 Vector3) Vector3
 
 @[c: 'Vector3Barycenter']
-pub fn vector3Barycenter(p Vector3, a Vector3, b Vector3, c Vector3) Vector3
+pub fn vector3_barycenter(p Vector3, a Vector3, b Vector3, c Vector3) Vector3
 
 @[c: 'Vector3Unproject']
-pub fn vector3Unproject(source Vector3, projection Matrix, view Matrix) Vector3
+pub fn vector3_unproject(source Vector3, projection Matrix, view Matrix) Vector3
 
 @[c: 'Vector3ToFloatV']
-pub fn vector3ToFloatV(v Vector3) Float3
+pub fn vector3_to_float_v(v Vector3) Float3
 
 @[c: 'Vector3Invert']
-pub fn vector3Invert(v Vector3) Vector3
+pub fn vector3_invert(v Vector3) Vector3
 
 @[c: 'Vector3Clamp']
-pub fn vector3Clamp(v Vector3, min Vector3, max Vector3) Vector3
+pub fn vector3_clamp(v Vector3, min Vector3, max Vector3) Vector3
 
 @[c: 'Vector3ClampValue']
-pub fn vector3ClampValue(v Vector3, min f32, max f32) Vector3
+pub fn vector3_clamp_value(v Vector3, min f32, max f32) Vector3
 
 @[c: 'Vector3Equals']
-pub fn vector3Equals(p Vector3, q Vector3) int
+pub fn vector3_equals(p Vector3, q Vector3) int
 
 @[c: 'Vector3Refract']
-pub fn vector3Refract(v Vector3, n Vector3, r f32) Vector3
+pub fn vector3_refract(v Vector3, n Vector3, r f32) Vector3
 
 @[c: 'MatrixDeterminant']
 pub fn matrix_determinant(mat Matrix) f32
@@ -325,7 +325,7 @@ pub fn quaternion_nlerp(q1 Quaternion, q2 Quaternion, amount f32) Quaternion
 pub fn quaternion_slerp(q1 Quaternion, q2 Quaternion, amount f32) Quaternion
 
 @[c: 'QuaternionFromVector3ToVector3']
-pub fn quaternion_from_vector3ToVector3(from Vector3, to Vector3) Quaternion
+pub fn quaternion_from_vector3_to_vector3(from Vector3, to Vector3) Quaternion
 
 @[c: 'QuaternionFromMatrix']
 pub fn quaternion_from_matrix(mat Matrix) Quaternion
